bc3b22d407dc44d3944f633fd9c0ab59da96ff34
Riyad <yousufh291@gmail.com>
2023-09-04 11:33:21 +0600

    Add functionality for buyHealth function

--- script.js
+++ script.js
@@ -67,7 +67,14 @@ function goStore() {
 }
 
 function buyHealth() {
-
+    if(gold <= 10){
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You Don't Have Enough Gold";
+    }
 }
 
 function buyWeapon() {

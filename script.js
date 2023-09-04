032954ee2b25a564fdebb0e4815146037bc60687
Riyad <yousufh291@gmail.com>
2023-09-04 09:26:51 +0600

    Update Location Function and Create A Location Object

--- script.js
+++ script.js
@@ -18,43 +18,50 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterNameText = document.querySelector("#monsterName");
 const monsterHealthTxt = document.querySelector("#monsterHealth");
 
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go TO Store", "Go To Cave", "Fight Dragon"],
+        "button function": [goStore, goCave, fightDragon],
+        text: "You Are In The Town Square. You Will See Sign For \"Store\""
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 Health (10 Gold)", "Buy weapon (30 Gold)", "Go To Town Square"],
+        "button function": [buyHealth, buyWeapon, goTown],
+        text: "You Are In The Store"
+    }
+];
+
 // Initialize Button
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function goTown(){
-    button1.innerText = "Go TO Store";
-    button2.innerText = "Go To Cave";
-    button3.innerText = "Fight Dragon";
+function location() {
+    button1.innerText = locations["button text"][0];
+    button2.innerText = locations["button text"][1];
+    button3.innerText = locations["button text"][2];
 
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon
+    button1.onclick = locations["button function"][0];
+    button2.onclick = locations["button function"][1];
+    button3.onclick = locations["button function"][2];
 
-    text.innerText = "You Are In The Town Square. You Will See Sign For Store";
+    text.innerText = locations.text;
 }
 
-function goStore(){
-    button1.innerText = "Buy 10 Health (10 Gold)";
-    button2.innerText = "Buy weapon (30 Gold)";
-    button3.innerText = "Go To Town Square";
+function goTown() {
+    update(locations[0]);
+}
 
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-
-    text.innerText = "Welcome To The Store";
+function goStore() {
+    
 }
-function goCave(){
-    console.log("Going To Cave");
+function goCave() {
+  // console.log("Going To Cave");
 }
-function fightDragon(){
-    console.log("Fighting With Dragon");
+function fightDragon() {
+  // console.log("Fighting With Dragon");
 }
-function buyHealth(){
-
-}
-function buyWeapon(){
-
-}
+function buyHealth() {}
+function buyWeapon() {}
